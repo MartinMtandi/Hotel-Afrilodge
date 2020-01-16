01746c817fd627434a231ad8a71624e5ca1e9813
Martin Mtandi <martin.mtandi@contitouch.co.zw>
2020-01-16 16:32:08 +0200

    chat bot

--- contact.php
+++ contact.php
@@ -122,21 +122,21 @@
             $("#display_results").html(data);											
         }); }); });
 
-
+</script>
     
 </body>
-</script>  
+  
 <!----------------------------------Mail Plugin By Peter Mbwkeu --------------------->
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5e2068f227773e0d832ddf45/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5e2068f227773e0d832ddf45/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
 </script>
 </html>
